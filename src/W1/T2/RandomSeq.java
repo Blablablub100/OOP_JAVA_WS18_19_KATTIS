ffcc49e2e98d6bd22ffa689e2942a292f7d9319c
frame94 <nikodemus94@gmail.com>
2018-10-18 15:58:05 +0200

    Author changed

--- src/W1/T2/RandomSeq.java
+++ src/W1/T2/RandomSeq.java
@@ -1,11 +1,13 @@
 package W1.T2;
 
+import java.util.Random;
+
 /**
  * Advanced Object Oriented Programming with Java, WS 2018
  * Problem: Quadratic.java
  * Link: http://introcs.cs.princeton.edu/java/12types/Quadratic.java.html
  * @author Clemens Tisch
- * @author YYY
+ * @author Nikodemus Kochanek
  * @version 1.0, 10/23/2018
  *
  * Method : ???
@@ -22,6 +24,7 @@ public class RandomSeq {
         // generate and print n numbers between 0 and 1
         for (int i = 0; i < n; i++) {
             System.out.println(Math.random());
+
         }
     }
 }
