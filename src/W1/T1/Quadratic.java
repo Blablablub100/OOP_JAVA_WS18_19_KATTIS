74b66215c727d5a7f352cd0f44581b722a7ceae8
blablablub100 <clemenstisch@hotmail.de>
2018-10-18 14:29:22 +0200

    Initial commit

---
+++ src/W1/T1/Quadratic.java
@@ -0,0 +1,33 @@
+package W1.T1;
+
+/**
+ * Advanced Object Oriented Programming with Java, WS 2018
+ * Problem: Quadratic.java
+ * Link: http://introcs.cs.princeton.edu/java/12types/Quadratic.java.html
+ * @author Clemens Tisch
+ * @author YYY
+ * @version 1.0, 10/23/2018
+ *
+ * Method : ???
+ * Status : ???
+ * Runtime: ???
+ */
+
+public class Quadratic {
+
+     public static void main(String[] args) {
+         double b = Double.parseDouble(args[0]);
+         double c = Double.parseDouble(args[1]);
+
+         double discriminant = b * b - 4.0 * c;
+
+         double sqroot = Math.sqrt(discriminant);
+
+         double root1 = (-b + sqroot) / 2.0;
+         double root2 = (-b - sqroot) / 2.0;
+
+         System.out.println(root1);
+         System.out.println(root2);
+
+     }
+}
---
+++ src/W1/T2/RandomSeq.java
@@ -0,0 +1,27 @@
+package W1.T2;
+
+/**
+ * Advanced Object Oriented Programming with Java, WS 2018
+ * Problem: Quadratic.java
+ * Link: http://introcs.cs.princeton.edu/java/12types/Quadratic.java.html
+ * @author Clemens Tisch
+ * @author YYY
+ * @version 1.0, 10/23/2018
+ *
+ * Method : ???
+ * Status : ???
+ * Runtime: ???
+ */
+
+public class RandomSeq {
+    public static void main(String[] args) {
+
+        // command-line argument
+        int n = Integer.parseInt(args[0]);
+
+        // generate and print n numbers between 0 and 1
+        for (int i = 0; i < n; i++) {
+            System.out.println(Math.random());
+        }
+    }
+}
