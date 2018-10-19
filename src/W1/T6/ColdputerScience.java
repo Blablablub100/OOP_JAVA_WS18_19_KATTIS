fe97d30e2da9a76bd30c84dc35ff61b030505b10
frame94 <nikodemus94@gmail.com>
2018-10-19 10:40:51 +0200

    T4 added main string args
    T6 added
    
    T6 HelloWorld added
    T6 HelloWorld debugged
    T6 HelloWorld finished
    
    T6 ColdputerScience added

---
+++ src/W1/T6/ColdputerScience.java
@@ -0,0 +1,23 @@
+package W1.T6;
+
+/**
+ * Advanced Object Oriented Programming with Java, WS 2018
+ * Problem: ColdputerScience.java
+ * Link:
+ * @author Clemens Tisch
+ * @author Nikodemus Kochanek
+ * @version 1.0, 10/23/2018
+ *
+ * Method : ???
+ * Status : ???
+ * Runtime: ???
+ */
+
+public class ColdputerScience {
+    public static void main(String[] args) {
+
+        int a = Integer.parseInt(args[0]);
+
+
+    }
+}
