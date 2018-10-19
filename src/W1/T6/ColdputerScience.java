1569dd45354514930aeceb0441866323cf9d2483
frame94 <nikodemus94@gmail.com>
2018-10-19 11:23:44 +0200

    T6 ColdputerScience added

--- src/W1/T6/ColdputerScience.java
+++ src/W1/T6/ColdputerScience.java
@@ -1,5 +1,7 @@
 package W1.T6;
 
+import java.util.Scanner;
+
 /**
  * Advanced Object Oriented Programming with Java, WS 2018
  * Problem: ColdputerScience.java
@@ -16,8 +18,21 @@ package W1.T6;
 public class ColdputerScience {
     public static void main(String[] args) {
 
-        int a = Integer.parseInt(args[0]);
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
 
+        int n = sc1.nextInt();
+        int cnt = 0;
 
+        String tempString = sc2.nextLine();
+        String[]  temps = tempString.split(" ");
+
+        for (int i = 0; i < n; i++) {
+            if (Integer.parseInt(temps[i]) < 0) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
     }
 }
