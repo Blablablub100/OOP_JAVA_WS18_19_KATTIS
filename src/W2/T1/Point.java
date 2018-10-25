e03619c1a3b355ce679524e593fa8ff223b64023
frame94 <nikodemus94@gmail.com>
2018-10-25 14:18:02 +0200

    T1 Rectangle added

--- src/W2/T1/Point.java
+++ src/W2/T1/Point.java
@@ -1,4 +1,13 @@
 package W2.T1;
 
 public class Point {
+    public int x = 0;
+    public int y = 0;
+    //constructor
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+
 }
+
