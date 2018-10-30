8e237ee34c19fc39b7f3e9657b586d7cb25aefac
frame94 <nikodemus94@gmail.com>
2018-10-30 11:16:49 +0100

    W2 T4 Planina added W2 T4 Planina debugged W2 T4 Planina commented W2 T4 Planina finished

---
+++ src/W2/T4/Planina.java
@@ -0,0 +1,45 @@
+package W2.T4;
+
+import java.util.Scanner;
+
+/**
+ * Advanced Object Oriented Programming with Java, WS 2018
+ * Problem: Planina.java Output of the number of points stored after N iterations
+ * Link: https://open.kattis.com/contests/pp5rtp/problems/planina
+ * @author Clemens Tisch
+ * @author Nikodemus Kochanek
+ * @version 1.0, 10/30/2018
+ *
+ * Method: ???
+ * Status: Accepted
+ * Runtime: 0.07
+ */
+
+public class Planina {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int y = 2;
+        int N;
+        int res;
+        boolean run;
+
+        do {
+            run = false;
+
+            N = sc.nextInt();
+            sc.nextLine();      // clears keyboard buffer
+
+            if(N < 1 || N > 15) run = true;
+        } while(run);
+
+        // calculates how many points are on one line
+        for(int i = 0; i < N; i++) {
+            y = (y * 2) - 1;
+        }
+
+        // calculates the amount of points that need to be saved
+        res = y * y;
+        System.out.println(res);
+    }
+}
