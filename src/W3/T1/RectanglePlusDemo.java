f2a2e396e32e7ace968b3af2d6cea2d90e28bce1
frame94 <nikodemus94@gmail.com>
2018-11-06 09:35:11 +0100

    W3 T1 RectanglePlus.java added W3 T1 RectanglePlusDemo.java added W3 T1 Relatable.java added
    W3 T6 ARealChanllenge.java added
    W3 T6 ARealChanllenge.java debugged
    W3 T6 ARealChanllenge.java accepted
    W3 T6 ARealChanllenge.java commented
    W3 T6 ARealChanllenge.java finished
    
    W3 T6 BoatParts.java added
    W3 T6 BoatParts.java debugged
    W3 T6 BoatParts.java accepted
    W3 T6 BoatParts.java commented
    W3 T6 BoatParts.java finished

---
+++ src/W3/T1/RectanglePlusDemo.java
@@ -0,0 +1,30 @@
+package W3.T1;
+
+import W2.T1.Rectangle;
+import org.w3c.dom.css.Rect;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class RectanglePlusDemo implements Cloneable {
+    public static void main(String[] args) {
+        Point p1 = new Point(5, 5);
+        RectanglePlus RP1 = new RectanglePlus();
+        RectanglePlus RP2 = new RectanglePlus(new Point(2,3));
+        RectanglePlus RP3 = new RectanglePlus(5,7);
+        RectanglePlus RP4 = new RectanglePlus(p1, 5, 7);
+
+        RP1.move(-2, -3);
+        System.out.println("RP1 new location: x = " + RP1.origin.x + ", y = " +RP1.origin.y);
+
+        System.out.println("Area of RP3: " + RP3.getArea());
+        System.out.println("Area of RP4: " + RP4.getArea());
+        if(RP3.isLargerThan(RP4) == -1) System.out.println("RP4 is larger than RP3.");
+        else if(RP3.isLargerThan(RP4) == 0) System.out.println("RP3 and RP4 have the same size.");
+        else System.out.println("RP3 is larger than RP4.");
+
+    }
+}
+
+
+
