58dbd0d619180f8b6d8e8097db35d1d53227163e
frame94 <nikodemus94@gmail.com>
2018-11-08 09:16:18 +0100

    Left beehind finished, submitted and commented Worked on rectangle, not quite functioning though

--- src/W3/T1/RectanglePlusDemo.java
+++ src/W3/T1/RectanglePlusDemo.java
@@ -4,9 +4,8 @@ import W2.T1.Rectangle;
 import org.w3c.dom.css.Rect;
 
 import java.awt.*;
-import java.util.Scanner;
 
-public class RectanglePlusDemo implements Cloneable {
+public class RectanglePlusDemo {
     public static void main(String[] args) {
         Point p1 = new Point(5, 5);
         RectanglePlus RP1 = new RectanglePlus();
@@ -14,14 +13,18 @@ public class RectanglePlusDemo implements Cloneable {
         RectanglePlus RP3 = new RectanglePlus(5,7);
         RectanglePlus RP4 = new RectanglePlus(p1, 5, 7);
 
+        System.out.println("RP1 location: x = " + RP1.origin.x + ", y = " + RP1.origin.y);
         RP1.move(-2, -3);
         System.out.println("RP1 new location: x = " + RP1.origin.x + ", y = " +RP1.origin.y);
+
+        System.out.println("RP2 location: x = " + RP2.origin.x + ", y = " + RP2.origin.y);
 
         System.out.println("Area of RP3: " + RP3.getArea());
         System.out.println("Area of RP4: " + RP4.getArea());
         if(RP3.isLargerThan(RP4) == -1) System.out.println("RP4 is larger than RP3.");
         else if(RP3.isLargerThan(RP4) == 0) System.out.println("RP3 and RP4 have the same size.");
         else System.out.println("RP3 is larger than RP4.");
+
 
     }
 }
---
+++ src/W3/T6/LeftBeehind.java
@@ -0,0 +1,59 @@
+package W3.T6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Advanced Object Oriented Programming with Java, WS 2018
+ * Problem: LeftBeehind.java decides if Bill can go to the convention
+ * Link: https://open.kattis.com/contests/ww2rp4/problems/leftbeehind
+ * @author Clemens Tisch
+ * @author Nikodemus Kochanek
+ * @version 1.0, 11/08/2018
+ *
+ * Method : ???
+ * Status : Accepted
+ * Runtime: 0.07
+ */
+
+public class LeftBeehind {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> res = new ArrayList<>();
+        List<Integer> x = new ArrayList<>();
+        List<Integer> y = new ArrayList<>();
+        int cnt = 0;
+
+
+        // runs until terminator input by user
+        do {
+            // user input
+            String[] tmp = new String[2];
+            tmp[0] = sc.nextLine();
+            tmp = tmp[0].split(" ");
+            x.add(Integer.valueOf(tmp[0]));
+            y.add(Integer.valueOf(tmp[1]));
+
+            // checks which output is needed
+            if(x.get(cnt) + y.get(cnt) == 13) res.add(3);
+            else if(x.get(cnt) > y.get(cnt)) res.add(1);
+            else if(x.get(cnt) < y.get(cnt)) res.add(2);
+            else if(x.get(cnt).equals(y.get(cnt))) res.add(4);
+
+            cnt++;
+        } while(!(x.get(cnt-1) == 0 && y.get(cnt-1) == 0));
+
+        sc.close();
+        cnt--;
+
+        // console output
+        for(int i = 0; i < cnt; i++) {
+            if(res.get(i) == 1) System.out.println("To the convention.");
+            else if(res.get(i) == 2) System.out.println("Left beehind.");
+            else if(res.get(i) == 3) System.out.println("Never speak again.");
+            else if(res.get(i) == 4) System.out.println("Undecided.");
+        }
+    }
+}
